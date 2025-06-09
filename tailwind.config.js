a61ed3b312d055f9a48faae9da45e18237390c9b
Laila Qadriyah <lailaqdr04@gmail.com>
2025-06-09 19:52:48 +0700

    edit by Laila

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  content: ["./views/**/*.{ejs,html,js}", "./public/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
+        sans: ['Inter', 'sans-serif'], 
+      }
     },
   },
+  plugins: [],
 }
