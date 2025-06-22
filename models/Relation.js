b174bce6a1c170d774d050cc35f8ba2c45b77e7f
Laila Qadriyah <lailaqdr04@gmail.com>
2025-06-22 12:26:41 +0700

    relasi blum done

--- models/Relation.js
+++ models/Relation.js
@@ -1,16 +1,29 @@
 const { Aset } = require('./AsetModel');
 const { Ruangan } = require('./RuanganModel');
+const { Asisten } = require('./Asistenmodel');
+
+Asisten.belongsTo(Ruangan, {
+  foreignKey: 'kode_ruangan',
+  as: 'ruangan' 
+});
+
+Ruangan.hasMany(Asisten, {
+  foreignKey: 'kode_ruangan',
+  as: 'asisten'
+});
 
 Aset.belongsTo(Ruangan, {
     foreignKey: 'kode_ruangan',
-    as: 'ruangan' // An item belongs to one room
+    as: 'ruangan' 
 });
 
 Ruangan.hasMany(Aset, {
     foreignKey: 'kode_ruangan',
-    as: 'aset' // A room can have many items
+    as: 'aset'
 });
 
 module.exports = { 
     Aset, 
-    Ruangan };
+    Ruangan,
+    Asisten
+};
